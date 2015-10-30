a8c190c13dfd23c31c862bac598b20bf62d95510
Torchid <rachel.l.popa@gmail.com>
2015-10-29 23:07:47 -0600

    refactor: Move page information into details view file

--- controller/Details.controller.js
+++ controller/Details.controller.js
@@ -1,16 +1,18 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller",
-   "sap/ui/model/resource/ResourceModel"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/resource/ResourceModel"
 ], function (Controller, ResourceModel) {
-   "use strict";
-   return Controller.extend("sap.ui.sunshine.wt.controller.Details", {
-     onInit : function () {
-     // set i18n model on view
-         var i18nModel = new ResourceModel({
-            bundleName: "sap.ui.sunshine.wt.i18n.i18n"
-        });
-         this.getView().setModel(i18nModel, "i18n");
-         
-      }
+    "use strict";
+    return Controller.extend("sap.ui.sunshine.wt.controller.Details", {
+        onInit : function () {
+            // set i18n model on view
+            var i18nModel = new ResourceModel({
+                bundleName: "sap.ui.sunshine.wt.i18n.i18n"
+            });
+            this.getView().setModel(i18nModel, "i18n");             
+        },
+        onNavPress : function () {
+            app.back();
+        }
    });
 });
