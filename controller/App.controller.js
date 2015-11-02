d1b0de01db0380aaeb33a534f9abfb7c3a57b268
Torchid <rachel.l.popa@gmail.com>
2015-11-01 21:20:59 -0600

    refactor: Apply language resources to entire app during component configuration

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,8 +1,7 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/resource/ResourceModel",
 	"sap/m/MessageToast"
-], function (Controller, ResourceModel, MessageToast) {
+], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("sap.ui.sunshine.wt.controller.App", {
         onInit : function () {
